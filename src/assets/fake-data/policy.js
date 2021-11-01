65c3619645c75c101ae310c527762e9c3d508e14
Andres Nguyen <thuongnguyen.it78@gmail.com>
2021-11-01 21:14:50 +0700

    Set up project

---
+++ src/assets/fake-data/policy.js
@@ -0,0 +1,24 @@
+const policy = [
+    {
+        name: "Shoppe",
+        description: "changchang2612",
+        icon: "bx bx-shopping-bag"
+    },
+    {
+        name: "Giao hàng",
+        description: "Ship CODE toàn quốc",
+        icon: "bx bx-credit-card"
+    },
+    {
+        name: "Page facebook",
+        description: "Ưu đãi dành cho khách hàng VIP",
+        icon: "bx bx-diamond"
+    },
+    {
+        name: "Hỗ trợ bảo hành",
+        description: "Đổi, sửa đồ tại tất cả store",
+        icon: "bx bx-donate-heart"
+    }
+]
+
+export default policy
